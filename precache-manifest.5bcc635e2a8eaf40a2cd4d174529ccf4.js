1add181e8ccbf3880da78f96081342e568647c78
Boaardz56 <jackie.lopez.56@gmail.com>
2020-08-15 05:26:10 -0400

    Updates

---
+++ precache-manifest.5bcc635e2a8eaf40a2cd4d174529ccf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18f5bd7ba93a040346bbdc06361cfcaa",
+    "url": "/userdirectory/index.html"
+  },
+  {
+    "revision": "e86582e61dd3c6ac2a0f",
+    "url": "/userdirectory/static/css/main.316e0a2e.chunk.css"
+  },
+  {
+    "revision": "cf732e45f5c7f6e020b8",
+    "url": "/userdirectory/static/js/2.f7b6befe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/userdirectory/static/js/2.f7b6befe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e86582e61dd3c6ac2a0f",
+    "url": "/userdirectory/static/js/main.5209e5fd.chunk.js"
+  },
+  {
+    "revision": "089fb84e23096f9887c6",
+    "url": "/userdirectory/static/js/runtime-main.a80afac8.js"
+  }
+]);
